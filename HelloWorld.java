81b43694b275b05b6a1681a78e117235091b1ce6
jma289 <37608449+jma289@users.noreply.github.com>
2018-03-20 22:04:35 -0700

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+// HelloWorld.java
+// Jia-hui Ma
+// jma289
+// lab1
+// prints greeting and some system information
+class HelloWorld{
+   public static void main(String[] args){
+      System.out.println("Hello, my name is Jia-hui Ma.");
+   }
+}
